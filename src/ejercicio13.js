cd082326079d2b97be4ffcaf12e047c4f0a18b19
elias tolaba <eliasgamer3512@gmail.com>
2025-04-13 23:38:04 -0300

    ejercicios realizados 7,11,12 y 13 por EliasRubenTolaba

---
+++ src/ejercicio13.js
@@ -0,0 +1,11 @@
+// EJERCICIO 13: Mostrar todos los elementos de un arreglo
+function mostrarArreglo() {
+
+    // Se crea un arreglo llamado 'numeros' con 5 elementos
+    let numeros = [10, 20, 30, 40, 50];
+
+    // Bucle que recorre el arreglo desde la posición 0 hasta el final
+    for (let i = 0; i < numeros.length; i++) {
+        console.log("Número en posición " + i + " es: " + numeros[i]);
+    }
+}
